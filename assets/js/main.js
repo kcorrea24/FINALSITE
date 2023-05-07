691443550e46a786c063984793588e20d855d39d
Karl Arnold Correa <karlarnoldcorrea24@gmail.com>
2023-05-07 19:26:08 +0800

    FINALOUTPUT

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,13 @@ window.addEventListener('load', () => {
    }, 600);
 });
 
-
+document.addEventListener("click",function (e){
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+      myModal.show();
+  }
+})
 
 var scroll = new SmoothScroll('a[href*="#"]');
-
-
